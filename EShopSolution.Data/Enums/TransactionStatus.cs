713850426d6e5d1177f041b207eedabd5b23fc73
thanhhoai <1851120014@sv.ut.edu.vn>
2021-07-15 08:37:12 +0700

    Add Entity

---
+++ EShopSolution.Data/Enums/TransactionStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShopSolution.Data.Enums
+{
+    namespace eShopSolution.Data.Enums
+    {
+        public enum TransactionStatus
+        {
+            Success,
+            Failed
+        }
+    }
